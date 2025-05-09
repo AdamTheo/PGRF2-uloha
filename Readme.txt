6e122b1953d0e09035d1bf3df6101dbc3daa1618
theod <theodor.adam88@gmail.com>
2025-05-09 17:56:21 +0200

    Finished reformating, small fixes. Added readme

---
+++ Readme.txt
@@ -0,0 +1,7 @@
+Main (App) najdete v Projekt v src.
+Případně lze použít zde spustitelný .exe soubor.
+Pro jeho správnou funkčnost bylo nutné přibalit lwjgl sem.
+
+Ovládání je přímo v aplikaci.
+Pohyb věže je nezávislý na pohybu podvozku, vždy zůstane taková, jakou ji necháte.
+Při opuštění středu jste vrácen na počátek.
--- src/Projekt/Renderer.java
+++ src/Projekt/Renderer.java
@@ -6,17 +6,18 @@ import Projekt.Solid.Solid;
 import Projekt.Solid.Tank;
 import Projekt.Solid.TankTurret;
 import Projekt.global.AbstractRenderer;
-import Projekt.model.*;
+import Projekt.model.Part;
+import Projekt.model.Vertex;
 import lwjglutils.OGLTexture2D;
 import org.lwjgl.glfw.GLFWKeyCallback;
+
+import java.io.IOException;
+import java.util.List;
 
 import static Projekt.global.GluUtils.gluLookAt;
 import static Projekt.global.GluUtils.gluPerspective;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
-
-import java.io.IOException;
-import java.util.List;
 
 public class Renderer extends AbstractRenderer {
     float angle;
@@ -30,7 +31,7 @@ public class Renderer extends AbstractRenderer {
     float turretSpeed = 0.7f;
     int fireTime = 20;
     //End of configurable parameters
-    int fireTimer =0;
+    int fireTimer = 0;
 
     float tankX = 0.2f;
     float tankY = 0.1f;
@@ -38,17 +39,14 @@ public class Renderer extends AbstractRenderer {
 
     float explosionX = 0;
     float explosionY = 0;
-    private float explosionZ = 0.24f;
-
     //Textures
-     OGLTexture2D groundTexture;
-     OGLTexture2D skyBox_back;
+    OGLTexture2D groundTexture;
+    OGLTexture2D skyBox_back;
     OGLTexture2D skyBox_bottom;
     OGLTexture2D skyBox_front;
     OGLTexture2D skyBox_left;
     OGLTexture2D skyBox_right;
     OGLTexture2D skyBox_top;
-
     boolean Wpressed = false;
     boolean Spressed = false;
     boolean Apressed = false;
@@ -59,6 +57,7 @@ public class Renderer extends AbstractRenderer {
     Solid tankTurret;
     Solid tank;
     Solid explosion;
+    private final float explosionZ = 0.24f;
 
     public Renderer() {
         super();
@@ -158,45 +157,47 @@ public class Renderer extends AbstractRenderer {
 
     }
 
-    public void drawAxes(){
+    public void drawAxes() {
         glColor3f(1.0f, 1.0f, 1.0f);
         glBegin(GL_LINES);
         glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex3f(0.0f, 0.0f,0.0f);
-        glVertex3f(1f, 0.0f,0.0f);
+        glVertex3f(0.0f, 0.0f, 0.0f);
+        glVertex3f(1f, 0.0f, 0.0f);
 
         glColor3f(0.0f, 1.0f, 0.0f);
-        glVertex3f(0.0f,0.0f,0.0f);
-        glVertex3f(0.0f,1.0f,0.0f);
+        glVertex3f(0.0f, 0.0f, 0.0f);
+        glVertex3f(0.0f, 1.0f, 0.0f);
 
         glColor3f(0.0f, 0.0f, 1.0f);
-        glVertex3f(0.0f,0.0f,0.0f);
-        glVertex3f(0.0f,0.0f,1.0f);
+        glVertex3f(0.0f, 0.0f, 0.0f);
+        glVertex3f(0.0f, 0.0f, 1.0f);
         glEnd();
 
     }
-    public void drawFloor(){
+
+    public void drawFloor() {
         glColor3f(1.0f, 1.0f, 1.0f);
         glEnable(GL_TEXTURE_2D);
         groundTexture.bind();
         glBegin(GL_QUADS);
         //glNormal3f(0,0,1);
         glTexCoord2f(0, 0);
-        glVertex3f(-5f,-5f,-0.01f);
+        glVertex3f(-5f, -5f, -0.01f);
 
         glTexCoord2f(0, 1);
-        glVertex3f(-5f,5f,-0.01f);
+        glVertex3f(-5f, 5f, -0.01f);
 
         glTexCoord2f(1, 1);
-        glVertex3f(5f,5f,-0.01f);
+        glVertex3f(5f, 5f, -0.01f);
 
         glTexCoord2f(1, 0);
-        glVertex3f(5f, -5f,-0.01f);
+        glVertex3f(5f, -5f, -0.01f);
 
         glEnd();
         glDisable(GL_TEXTURE_2D);
     }
-    public void drawSkybox(){
+
+    public void drawSkybox() {
         glColor3f(1.0f, 1.0f, 1.0f);
         glEnable(GL_TEXTURE_2D);
         glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
@@ -294,54 +295,55 @@ public class Renderer extends AbstractRenderer {
 
         glDisable(GL_TEXTURE_2D);
     }
-    public void drawSolid(Solid solid){
+
+    public void drawSolid(Solid solid) {
         glColor3f(1.0f, 1.0f, 1.0f);
-        for(int p = 0; p < solid.getPB().size(); p++){ // Geting part from partbuffer
+        for (int p = 0; p < solid.getPB().size(); p++) { // Geting part from partbuffer
             Part part = solid.getPB().get(p);
             int start = part.getStart();
             int length = part.getLength();
             String type = part.getName();
-            List <Vertex> vertexBuffer = solid.getVB();
-            List <Integer> indexBuffer = solid.getIB();
+            List<Vertex> vertexBuffer = solid.getVB();
+            List<Integer> indexBuffer = solid.getIB();
 
-            if(type.equals("Triangles")){
+            if (type.equals("Triangles")) {
                 glBegin(GL_TRIANGLES);
-                for(int i = 0; i < length ; i++){
-                    Vertex a = vertexBuffer.get(indexBuffer.get(start + i*3));
-                    Vertex b  = vertexBuffer.get(indexBuffer.get(start + i*3 +1));
-                    Vertex c = vertexBuffer.get(indexBuffer.get(start + i*3 +2));
+                for (int i = 0; i < length; i++) {
+                    Vertex a = vertexBuffer.get(indexBuffer.get(start + i * 3));
+                    Vertex b = vertexBuffer.get(indexBuffer.get(start + i * 3 + 1));
+                    Vertex c = vertexBuffer.get(indexBuffer.get(start + i * 3 + 2));
 
-                    glColor3f((float)a.getCol().getR(),(float)a.getCol().getG(),(float)a.getCol().getB());
-                    glVertex3f(a.getX(),a.getY(),a.getZ());
+                    glColor3f((float) a.getCol().getR(), (float) a.getCol().getG(), (float) a.getCol().getB());
+                    glVertex3f(a.getX(), a.getY(), a.getZ());
 
-                    glColor3f((float)b.getCol().getR(),(float)b.getCol().getG(),(float)b.getCol().getB());
-                    glVertex3f(b.getX(),b.getY(),b.getZ());
+                    glColor3f((float) b.getCol().getR(), (float) b.getCol().getG(), (float) b.getCol().getB());
+                    glVertex3f(b.getX(), b.getY(), b.getZ());
 
-                    glColor3f((float)c.getCol().getR(),(float)c.getCol().getG(),(float)c.getCol().getB());
-                    glVertex3f(c.getX(),c.getY(),c.getZ());
+                    glColor3f((float) c.getCol().getR(), (float) c.getCol().getG(), (float) c.getCol().getB());
+                    glVertex3f(c.getX(), c.getY(), c.getZ());
 
                 }
                 glEnd();
             }
-            if(type.equals("TriangeStrips")){
+            if (type.equals("TriangeStrips")) {
                 glBegin(GL_TRIANGLE_STRIP);
-                    Vertex a = vertexBuffer.get(indexBuffer.get(start));
-                    Vertex b  = vertexBuffer.get(indexBuffer.get(start +1));
-                    Vertex c = vertexBuffer.get(indexBuffer.get(start + 2));
+                Vertex a = vertexBuffer.get(indexBuffer.get(start));
+                Vertex b = vertexBuffer.get(indexBuffer.get(start + 1));
+                Vertex c = vertexBuffer.get(indexBuffer.get(start + 2));
 
-                    glColor3f((float)a.getCol().getR(),(float)a.getCol().getG(),(float)a.getCol().getB());
-                    glVertex3f(a.getX(),a.getY(),a.getZ());
+                glColor3f((float) a.getCol().getR(), (float) a.getCol().getG(), (float) a.getCol().getB());
+                glVertex3f(a.getX(), a.getY(), a.getZ());
 
-                    glColor3f((float)b.getCol().getR(),(float)b.getCol().getG(),(float)b.getCol().getB());
-                    glVertex3f(b.getX(),b.getY(),b.getZ());
+                glColor3f((float) b.getCol().getR(), (float) b.getCol().getG(), (float) b.getCol().getB());
+                glVertex3f(b.getX(), b.getY(), b.getZ());
 
-                    glColor3f((float)c.getCol().getR(),(float)c.getCol().getG(),(float)c.getCol().getB());
-                    glVertex3f(c.getX(),c.getY(),c.getZ());
+                glColor3f((float) c.getCol().getR(), (float) c.getCol().getG(), (float) c.getCol().getB());
+                glVertex3f(c.getX(), c.getY(), c.getZ());
 
-                for(int i = 3; i < length ; i++){
-                    Vertex d = vertexBuffer.get(indexBuffer.get(start+i));
-                    glColor3f((float)d.getCol().getR(),(float)d.getCol().getG(),(float)d.getCol().getB());
-                    glVertex3f(d.getX(),d.getY(),d.getZ());
+                for (int i = 3; i < length; i++) {
+                    Vertex d = vertexBuffer.get(indexBuffer.get(start + i));
+                    glColor3f((float) d.getCol().getR(), (float) d.getCol().getG(), (float) d.getCol().getB());
+                    glVertex3f(d.getX(), d.getY(), d.getZ());
                 }
                 glEnd();
 
@@ -349,77 +351,83 @@ public class Renderer extends AbstractRenderer {
         }
     }
 
-    public void movement(){
+    public void movement() {
 
-        if(Apressed && !Dpressed){
+        if (tankX > 4.9 || tankY > 4.9 || tankX < -4.9 || tankY < -4.9) {
+            tankX = 0;
+            tankY = 0;
+        }
+
+        if (Apressed && !Dpressed) {
             angle += rotatinspeed;
         }
-        if(!Apressed && Dpressed){
+        if (!Apressed && Dpressed) {
             angle -= rotatinspeed;
         }
 
-        if(Wpressed && !Spressed){
+        if (Wpressed && !Spressed) {
             velocity += acceleration;
-            if (velocity >= max_min_speed){
+            if (velocity >= max_min_speed) {
                 velocity = max_min_speed;
             }
-            if(velocity < 0){
-                velocity += acceleration *3;
+            if (velocity < 0) {
+                velocity += acceleration * 3;
             }// Stronger breaking
         }
 
-        if(!Wpressed && Spressed){
+        if (!Wpressed && Spressed) {
             velocity -= acceleration;
-            if (velocity <= -max_min_speed){
+            if (velocity <= -max_min_speed) {
                 velocity = -max_min_speed;
             }
-            if(velocity > 0){
-                velocity -= acceleration *3;
+            if (velocity > 0) {
+                velocity -= acceleration * 3;
             }// Stronger breaking
         }
 
-        if(!Wpressed && !Spressed){
-            if(velocity > 0){
+        if (!Wpressed && !Spressed) {
+            if (velocity > 0) {
                 velocity -= braking;
             }
-            if(velocity < 0){
+            if (velocity < 0) {
                 velocity += braking;
             }
         }
 
         //Angles and moving forward in specific direction
-        if(angle >= 360){
+        if (angle >= 360) {
             angle = 0;
         }
-        if(angle < 0){
+        if (angle < 0) {
             angle = 359.95f;
         }
 
-        float radians = (float)Math.toRadians(angle);
-        tankX =tankX + (float)(Math.cos(radians) * velocity);
-        tankY =tankY + (float)(Math.sin(radians) * velocity);
+        float radians = (float) Math.toRadians(angle);
+        tankX = tankX + (float) (Math.cos(radians) * velocity);
+        tankY = tankY + (float) (Math.sin(radians) * velocity);
 
 
     }
-    public void turretMovement(){
-        if(Mpressed && !Npressed){
+
+    public void turretMovement() {
+        if (Mpressed && !Npressed) {
             turretAngle -= turretSpeed;
         }
-        if(!Mpressed && Npressed){
+        if (!Mpressed && Npressed) {
             turretAngle += turretSpeed;
         }
-        if(turretAngle >= 360){
+        if (turretAngle >= 360) {
             turretAngle = 0;
         }
-        if(turretAngle < 0){
+        if (turretAngle < 0) {
             turretAngle = 359.99f;
         }
 
         //For shooting purposes
 
-        float radians = (float)Math.toRadians(turretAngle);
-        explosionX =tankX + (float)(Math.cos(radians) * 0.18f);
-        explosionY =tankY + (float)(Math.sin(radians) * 0.18f);
+        float radians = (float) Math.toRadians(turretAngle);
+        explosionX = tankX + (float) (Math.cos(radians) * 0.18f);
+        explosionY = tankY + (float) (Math.sin(radians) * 0.18f);
     }
 
     @Override
@@ -434,7 +442,7 @@ public class Renderer extends AbstractRenderer {
 
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
-        gluLookAt(1.3f, 1.3f, 0.9f, tankX, tankY, tankZ, 0, 0, 1);
+        gluLookAt(1.3f, 1.3f, 1.5f, tankX, tankY, tankZ, 0, 0, 1);
         glPushMatrix();
         glPushMatrix();
 
@@ -443,19 +451,19 @@ public class Renderer extends AbstractRenderer {
         drawSkybox();
 
         movement();
-        glTranslatef(tankX, tankY, tankZ+0.11f);
-        glRotatef(angle,0,0,1);
+        glTranslatef(tankX, tankY, tankZ + 0.11f);
+        glRotatef(angle, 0, 0, 1);
         drawSolid(tank);
         glPopMatrix();
         turretMovement();
-        glTranslatef(tankX, tankY, tankZ+0.11f);
-        glRotatef(turretAngle,0,0,1);
+        glTranslatef(tankX, tankY, tankZ + 0.11f);
+        glRotatef(turretAngle, 0, 0, 1);
         drawSolid(tankTurret);
         glPopMatrix();
 
         //Only firing effect nad projectile
-        if(fireTimer > 0){
-            fireTimer --;
+        if (fireTimer > 0) {
+            fireTimer--;
             glTranslatef(explosionX, explosionY, explosionZ);
             drawSolid(explosion);
         }
@@ -463,11 +471,10 @@ public class Renderer extends AbstractRenderer {
         textRenderer.clear();
         textRenderer.addStr2D(width - 80, height - 60, "Pohyb WSAD");
         textRenderer.addStr2D(width - 160, height - 40, "Nataceni veze-M,N. Vystrel F");
-        textRenderer.addStr2D(width - 340, height -20, "PGRF2-Adam Theodor, Projekt Tank. Posledni editace 9.5.25");
+        textRenderer.addStr2D(width - 340, height - 20, "PGRF2-Adam Theodor, Projekt Tank. Posledni editace 9.5.25");
 
 
     }
-
 
 
 }
